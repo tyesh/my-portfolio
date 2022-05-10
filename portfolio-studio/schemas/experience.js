f183cc64a71bd55da306f6d77deaaeaeb0f0961a
carlos <carlos.velazquez91@gmail.com>
2022-05-10 11:34:08 -0400

    Added query for experience data

---
+++ portfolio-studio/schemas/experience.js
@@ -0,0 +1,32 @@
+export default {
+  name: "experience",
+  title: "Experience",
+  type: "document",
+  fields: [
+    {
+      name: "charge",
+      title: "Charge",
+      type: "string",
+    },
+    {
+      name: "location",
+      title: "Location",
+      type: "string",
+    },
+    {
+      name: "range",
+      title: "Range",
+      type: "string",
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "text",
+    },
+    {
+      name: "order",
+      title: "Order",
+      type: "number",
+    },
+  ],
+};
--- src/components/sections/MyExperience.js
+++ src/components/sections/MyExperience.js
@@ -1,79 +1,43 @@
 import React, { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import sanityCliend from "../../client.js";
+import sanityClient from "../../client.js";
 
 const MyExperience = () => {
   const [experiences, setExperiences] = useState(null);
 
-  /*useEffect(() => {
-        sanityCliend.fetch(`*[]{
-            name,
-            description
-            fromDate,
-            toDate,
-            mainImage{
-                asset->{
-                    _id,
-                    url
-                },
-                alt
-            }
-        }`)
-        .then(data => setExperiences(data))
-        .catch(error => console.log(error));
-    }, []);*/
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "experience"] | order(order desc) {
+            charge,
+            description,
+            range,
+            location
+        }`
+      )
+      .then((data) => {
+        setExperiences(data);
+        console.log(data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
   return (
     <Container className="my-5">
-        <h2>My Experience</h2>
+      <h2>My Experience</h2>
       <Row className="row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-        <Col>
-          <div className="card border-secondary mb-3">
-            <div className="card-header">2010</div>
-            <div className="card-body">
-              <h4 className="card-title">Secondary card title</h4>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
+        {experiences && experiences.map((experience, index) => (
+          <Col key={index}>
+            <div className="card border-secondary mb-3">
+              <div className="card-header">{experience.range}</div>
+              <div className="card-body">
+                <h4 className="card-title">{`${experience.charge} - ${experience.location}`}</h4>
+                <p className="card-text">
+                  {experience.description}
+                </p>
+              </div>
             </div>
-          </div>
-        </Col>
-        <Col>
-          <div className="card border-secondary mb-3">
-            <div className="card-header">2010</div>
-            <div className="card-body">
-              <h4 className="card-title">Secondary card title</h4>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
-          </div>
-        </Col>
-        <Col>
-          <div className="card border-secondary mb-3">
-            <div className="card-header">2010</div>
-            <div className="card-body">
-              <h4 className="card-title">Secondary card title</h4>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
-          </div>
-        </Col>
-        <Col>
-          <div className="card border-secondary mb-3">
-            <div className="card-header">2010</div>
-            <div className="card-body">
-              <h4 className="card-title">Secondary card title</h4>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
-          </div>
-        </Col>
+          </Col>
+        ))}
       </Row>
     </Container>
   );
