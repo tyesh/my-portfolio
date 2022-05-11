15d759daf40c6628fbc7cc740bff45f2931bcdc2
carlos <carlos.velazquez91@gmail.com>
2022-05-11 15:21:16 -0400

    Update footer content

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
         <Container>
             <Row>
                 <Col className="text-center py-3">
-                    Footer
+                    Carlos Velazquez - {new Date().getFullYear()}
                 </Col>
             </Row>
         </Container>
