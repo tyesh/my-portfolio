b1be0918f64d64c116a0c3cd0a0954dc669726a1
carlos <carlos.velazquez91@gmail.com>
2022-05-12 16:36:08 -0400

    Added contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import Header from './layout/Header';
+import Footer from "./layout/Footer";
+import { Col, Container, Row } from "react-bootstrap";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <>
+      <Header />
+      <main className="contact">
+        <Container>
+          <Row>
+            <Col>
+              <h1>Contact</h1>
+              <p>Feel free to send a email to <a href = "mailto: cvelazquez_91@hotmail.com">cvelazquez_91@hotmail.com</a> or add me to you Linkedin network to say hello.</p>
+            </Col>
+          </Row>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 };
 
